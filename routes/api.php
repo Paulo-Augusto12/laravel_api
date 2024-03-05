a30666e43b5a421d6c54f3f50b82b05534a131af
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-03-05 10:47:10 -0300

    feat: :sparkles: developed route to set task as completed or not

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Models\Task;
 use Illuminate\Http\Request;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
 Route::put('/v1/tasks/{id}', [TaskController::class, 'update']);
